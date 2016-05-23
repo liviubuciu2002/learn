7c57d444e9f3d3a6a40eb605ce7806ee4f5f6983
Your Name <you@example.com>
2016-05-23 07:27:06 +0100

    easy mock and other changes

---
+++ spring/spring-mvc/src/main/java/com/spring_mvc/WebConfig.java
@@ -0,0 +1,13 @@
+package com.spring_mvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	// Override configuration methods...
+
+}
---
+++ spring/spring-mvc/src/main/webapp/WEB-INF/myliviu/myview.jsp
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<body>
+<h3>My H3</h3>
+</body>
+</html>
